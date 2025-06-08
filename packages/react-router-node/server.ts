cf836dcf2288f8e938b3bc9d4c2d6e5c0ccb3be0
James Garbutt <43081j@users.noreply.github.com>
2025-06-08 22:11:58 +0100

    wip(react-router-node): expose `createMiddleware`

--- packages/react-router-node/server.ts
+++ packages/react-router-node/server.ts
@@ -1,4 +1,5 @@
-import type { RequestListener } from "node:http";
+import type { RequestListener, IncomingMessage, ServerResponse } from "node:http";
+import { TLSSocket } from "node:tls";
 
 import type {
   AppLoadContext,
@@ -9,6 +10,7 @@ import type {
 import { createRequestHandler } from "react-router";
 import type { ClientAddress } from "@mjackson/node-fetch-server";
 import { createRequestListener as createRequestListener_ } from "@mjackson/node-fetch-server";
+import { createReadableStreamFromReadable, writeReadableStreamToWritable } from "./stream";
 
 type MaybePromise<T> = T | Promise<T>;
 
@@ -38,4 +40,127 @@ export function createRequestListener(
     let loadContext = await options.getLoadContext?.(request, client);
     return handleRequest(request, loadContext);
   });
+}
+
+export type RequestHandler = (
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: (err?: unknown) => void
+) => Promise<void>;
+
+export function createMiddleware(
+  options: RequestListenerOptions
+): RequestHandler {
+  let handleRequest = createRequestHandler(options.build, options.mode);
+
+  return async (
+    req: IncomingMessage,
+    res: ServerResponse,
+    next: (err?: unknown) => void
+  ) => {
+    try {
+      let request = createRemixRequest(req, res);
+      let loadContext = await options.getLoadContext?.(req, res);
+
+      let response = await handleRequest(request, loadContext);
+
+      await sendRemixResponse(res, response);
+    } catch (error: unknown) {
+      // Express doesn't support async functions, so we have to pass along the
+      // error manually using next().
+      next(error);
+    }
+  };
+}
+
+export function createRemixRequest(
+  req: IncomingMessage,
+  res: ServerResponse
+): Request {
+  // req.hostname doesn't include port information so grab that from
+  // `X-Forwarded-Host` or `Host`
+  let [, hostnamePortStr] =
+    typeof req.headers["X-Forwarded-Host"] === "string"
+      ? req.headers["X-Forwarded-Host"].split(":")
+      : [];
+  let [, hostPortStr] = req.headers["host"]?.split(":") ?? [];
+  let hostnamePort = Number.parseInt(hostnamePortStr, 10);
+  let hostPort = Number.parseInt(hostPortStr, 10);
+  let port = Number.isSafeInteger(hostnamePort)
+    ? hostnamePort
+    : Number.isSafeInteger(hostPort)
+    ? hostPort
+    : "";
+  // Use req.hostname here as it respects the "trust proxy" setting
+  const protocol = req.socket instanceof TLSSocket ? "https" : "http";
+  const requestUrl = new URL(
+    `${protocol}://${req.headers.host}${req.url ?? ""}`
+  );
+  let resolvedHost = `${requestUrl.hostname}${port ? `:${port}` : ""}`;
+  // Use `req.originalUrl` so Remix is aware of the full path
+  let url = new URL(`${protocol}://${resolvedHost}${req.url}`);
+
+  // Abort action/loaders once we can no longer write a response
+  let controller: AbortController | null = new AbortController();
+  let init: RequestInit = {
+    method: req.method,
+    headers: createRemixHeaders(req.headers),
+    signal: controller.signal,
+  };
+
+  // Abort action/loaders once we can no longer write a response iff we have
+  // not yet sent a response (i.e., `close` without `finish`)
+  // `finish` -> done rendering the response
+  // `close` -> response can no longer be written to
+  res.on("finish", () => (controller = null));
+  res.on("close", () => controller?.abort());
+
+  if (req.method !== "GET" && req.method !== "HEAD") {
+    init.body = createReadableStreamFromReadable(req);
+    (init as { duplex: "half" }).duplex = "half";
+  }
+
+  return new Request(url.href, init);
+}
+
+function createRemixHeaders(
+  requestHeaders: IncomingMessage["headers"]
+): Headers {
+  let headers = new Headers();
+
+  for (let [key, values] of Object.entries(requestHeaders)) {
+    if (values) {
+      if (Array.isArray(values)) {
+        for (let value of values) {
+          headers.append(key, value);
+        }
+      } else {
+        headers.set(key, values);
+      }
+    }
+  }
+
+  return headers;
+}
+
+async function sendRemixResponse(
+  res: ServerResponse,
+  nodeResponse: Response
+): Promise<void> {
+  res.statusMessage = nodeResponse.statusText;
+  res.statusCode = nodeResponse.status;
+
+  for (let [key, value] of nodeResponse.headers.entries()) {
+    res.appendHeader(key, value);
+  }
+
+  if (nodeResponse.headers.get("Content-Type")?.match(/text\/event-stream/i)) {
+    res.flushHeaders();
+  }
+
+  if (nodeResponse.body) {
+    await writeReadableStreamToWritable(nodeResponse.body, res);
+  } else {
+    res.end();
+  }
 }
